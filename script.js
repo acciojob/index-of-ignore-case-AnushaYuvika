50ae7b8173bed1ee355a4a16a1d680311ca3e99d
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-07-25 23:45:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+	let n = s1.length; 
+	let m = s2.length;
+
+	for (let i=0; i<n-m; i++) {
+		String sub = s1.substring(i, i+m);
+		if (sub.equalsIgnoreCase(s2)) {
+			return i;
+		}
+	}
+
+	return -1;
+} 
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+const s2 = prompt("Enter s2:"); 
 alert(indexOfIgnoreCase(s1, s2));
